ae0b72b33224a321a07debcc5167515d679c5241
pheralb <pablohdez4340@gmail.com>
2023-04-03 00:16:25 +0100

    🛠️ Header improvements + update icons.

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,22 +1,25 @@
 import { ExternalLink } from "@/ui/link";
-import { IoLogoGithub, IoLogoTwitter } from "react-icons/io5";
+import { GitHub, Twitter, Droplet } from "iconoir-react";
 
 const Header = () => {
   return (
     <div className="fixed top-0 w-full py-5 px-6 flex items-center justify-between text-neutral-400">
+      <div className="flex items-center space-x-2">
+      <Droplet width={16} />
       <h3 className="font-medium">screenshot</h3>
-      <div className="flex items-center space-x-4">
+      </div>
+      <div className="flex items-center space-x-5">
         <ExternalLink
           href="https://twitter.com/pheralb_"
           className="text-neutral-400 hover:text-white duration-100 transition-colors"
         >
-          <IoLogoTwitter size={20} />
+          <Twitter width={20} />
         </ExternalLink>
         <ExternalLink
           href="https://github.com/pheralb/screenshot"
           className="text-neutral-400 hover:text-white duration-100 transition-colors"
         >
-          <IoLogoGithub size={20} />
+          <GitHub width={20} />
         </ExternalLink>
       </div>
     </div>
