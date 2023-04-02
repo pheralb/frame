7d7ef6a84dac25a63379c3c812058e9fb48720a5
pheralb <pablohdez4340@gmail.com>
2023-04-03 00:16:39 +0100

    🛠️ Update tailwind config.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        light: "#f5f5f5",
         dark: "#161616",
       },
       fontFamily: {
-        sans: ['var(--font-heebo)', 'sans-serif'],
+        sans: ["var(--font-heebo)", "sans-serif"],
+      },
+      fontSize: {
+        mini: "15px",
       },
     },
   },
