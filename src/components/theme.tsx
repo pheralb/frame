37063dc6ecca07ada5c2488b7b1de25c9964eea2
pheralb <pablohdez4340@gmail.com>
2023-04-25 00:36:48 +0100

    🎨 Design improvements.

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -1,11 +1,17 @@
 import { useTheme } from "next-themes";
 import { SunLight, HalfMoon } from "iconoir-react";
+import { cn } from "@/utils";
 
 const ChangeTheme = () => {
   const { theme, setTheme } = useTheme();
   return (
     <button
-      className="text-neutral-900 transition-colors duration-100 hover:text-dark dark:text-neutral-400 dark:hover:text-white"
+      className={cn(
+        "text-neutral-900 transition-colors duration-100 dark:text-neutral-400",
+        theme === "dark"
+          ? "dark:hover:text-yellow-400"
+          : "hover:text-indigo-900"
+      )}
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
     >
       {theme === "dark" ? <SunLight width={20} /> : <HalfMoon width={20} />}
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,6 +1,6 @@
 import ChangeTheme from "@/components/theme";
 import { ExternalLink } from "@/ui";
-import { GitHub, Twitter, Droplet } from "iconoir-react";
+import { GitHub, Twitter, Combine } from "iconoir-react";
 
 const iconSize = 20;
 
@@ -8,13 +8,14 @@ const Header = () => {
   return (
     <div className="fixed top-0 flex w-full items-center justify-between px-6 pt-6 text-neutral-400">
       <div className="flex items-center space-x-2">
-        <Droplet width={iconSize} />
-        <h3 className="font-medium">screenshot</h3>
+        <Combine width={iconSize} />
+        <h3 className="font-medium">frame</h3>
+        <span className="text-yellow-700">beta</span>
       </div>
       <div className="flex items-center space-x-5">
         <ExternalLink
           href="https://twitter.com/pheralb_"
-          className="text-neutral-900 transition-colors duration-100 hover:text-neutral-600 dark:text-neutral-400 dark:hover:text-white"
+          className="text-neutral-900 transition-colors duration-100 hover:text-blue-500 dark:text-neutral-400 dark:hover:text-blue-400"
         >
           <Twitter width={iconSize} />
         </ExternalLink>
