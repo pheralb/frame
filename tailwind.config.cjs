448e5e63bf0f45740a9bf175b37239a466335d03
pheralb <pablohdez4340@gmail.com>
2023-04-24 14:15:04 +0100

    🛠️ Add fontFamily settings + animations for radix dialog.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
@@ -9,10 +11,24 @@ module.exports = {
         dark: "#161616",
       },
       fontFamily: {
-        sans: ["var(--font-heebo)", "sans-serif"],
+        sans: ["var(--inter-font)", ...fontFamily.sans],
       },
       fontSize: {
         mini: "15px",
+      },
+      keyframes: {
+        overlayShow: {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+        contentShow: {
+          from: { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
+          to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
+        },
+      },
+      animation: {
+        overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
     },
   },
