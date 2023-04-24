dfa996bf2297527a22bb982492f7b7c2760f2605
pheralb <pablohdez4340@gmail.com>
2023-04-24 14:17:07 +0100

    🚀 Update settings using Zustand + design improvements.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,35 @@
 import { useRef, useState } from "react";
-import exportAsImage from "@/utils/exportAsImage";
-import clsx from "clsx";
+import { cn, exportAsImage } from "@/utils";
+import { toast } from "sonner";
 
 import { gradients } from "@/gradients";
-import { Button } from "@/ui/button";
-import Dropzone from "@/components/dropzone";
-import SidebarSection from "@/components/sidebarSection";
-import Sidebar from "@/components/sidebar";
+import { Button, Dialog } from "@/ui";
 
+import Dropzone from "@/components/dropzone";
+import { Sidebar, SidebarSection } from "@/components/sidebar";
+import { ChangePadding, ChangeRounded } from "@/components/settings";
 import { AddMediaImage, Download } from "iconoir-react";
+
+import { useImageSettings } from "@/store/settings";
 
 export default function Home() {
   const [gradient, setGradient] = useState<string>("");
   const getImage = useRef<HTMLDivElement>(null);
   const [image, setImage] = useState<File | null>(null);
+  const { padding, rounded } = useImageSettings((state) => ({
+    padding: state.padding,
+    rounded: state.rounded,
+  }));
 
-  const [padding, setPadding] = useState<number>(0);
-  const [rounded, setRounded] = useState<number>(0);
+  const divStyle = {
+    padding: `${padding}px`,
+    borderRadius: `${rounded}px`,
+  };
 
-  const paddingStyle = `p-[${padding}px]`;
-  const roundedStyle = `rounded-[${rounded}px]`;
-
-  console.log(paddingStyle, roundedStyle);
+  const handleDownload = () => {
+    exportAsImage(getImage.current as HTMLDivElement, "hello");
+    toast("Downloading image...");
+  };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
@@ -39,15 +47,22 @@ export default function Home() {
                 >
                   New image
                 </Button>
-                <Button
-                  wFull
-                  icon={<Download width={18} stroke="1" />}
-                  onClick={() =>
-                    exportAsImage(getImage.current as HTMLDivElement, "hello")
+                <Dialog
+                  title="Download image"
+                  btnIcon={<Download width={18} stroke="1" />}
+                  btnText="Download"
+                  action={
+                    <Button
+                      wFull
+                      icon={<Download width={18} stroke="1" />}
+                      onClick={handleDownload}
+                    >
+                      Download image
+                    </Button>
                   }
                 >
-                  Download image
-                </Button>
+                  asdas
+                </Dialog>
               </div>
             </SidebarSection>
             <SidebarSection title="Gradients">
@@ -56,7 +71,7 @@ export default function Home() {
                   <button
                     key={gradient}
                     onClick={() => setGradient(gradient)}
-                    className={clsx(
+                    className={cn(
                       "p-4 m-1 rounded-xl hover:-translate-y-0.5 duration-150 transition-all",
                       gradient
                     )}
@@ -65,39 +80,13 @@ export default function Home() {
               </div>
             </SidebarSection>
             <SidebarSection title="Settings">
-              <div className="flex flex-col space-y-1 mb-3">
-                <label htmlFor="padding">Padding:</label>
-                <div className="flex items-center space-x-2 w-full">
-                  <input
-                    id="padding"
-                    type="range"
-                    min="0"
-                    max="100"
-                    value={padding}
-                    onChange={(e) => setPadding(parseInt(e.target.value))}
-                  />
-                  <span className="text-neutral-500">{padding}px</span>
-                </div>
-              </div>
-              <div className="flex flex-col space-y-1">
-                <label htmlFor="rounded">Rounded:</label>
-                <div className="flex items-center space-x-2 w-full">
-                  <input
-                    id="rounded"
-                    type="range"
-                    min="0"
-                    max="100"
-                    value={rounded}
-                    onChange={(e) => setRounded(parseInt(e.target.value))}
-                  />
-                  <span className="text-neutral-500">{rounded}px</span>
-                </div>
-              </div>
+              <ChangePadding />
+              <ChangeRounded />
             </SidebarSection>
           </Sidebar>
           <div className="ml-64 m-3 flex flex-col">
             <div ref={getImage}>
-              <div className={clsx(gradient, paddingStyle, roundedStyle)}>
+              <div style={divStyle} className={cn(gradient)}>
                 <img
                   src={URL.createObjectURL(image)}
                   alt="image"
