701e151b00522074b8c8333b025648f56c6279c7
pheralb <pablohdez4340@gmail.com>
2023-04-25 23:39:57 +0100

    ⚙️ Change name store.

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -1,8 +1,8 @@
-import { useImageSettings } from "@/store/settings";
+import { useBackgroundSettings, useImageSettings } from "@/store/settings";
 
-export const ChangePadding = () => {
-  const padding = useImageSettings((state) => state.padding);
-  const setPadding = useImageSettings((state) => state.updatePadding);
+export const ChangeBgPadding = () => {
+  const padding = useBackgroundSettings((state) => state.padding);
+  const setPadding = useBackgroundSettings((state) => state.updatePadding);
   return (
     <div className="mb-3 flex flex-col space-y-1">
       <label htmlFor="padding">Padding:</label>
@@ -21,9 +21,9 @@ export const ChangePadding = () => {
   );
 };
 
-export const ChangeRounded = () => {
-  const rounded = useImageSettings((state) => state.rounded);
-  const setRounded = useImageSettings((state) => state.updateRounded);
+export const ChangeBgRounded = () => {
+  const rounded = useBackgroundSettings((state) => state.rounded);
+  const setRounded = useBackgroundSettings((state) => state.updateRounded);
   return (
     <div className="flex flex-col space-y-1">
       <label htmlFor="rounded">Rounded:</label>
@@ -37,6 +37,28 @@ export const ChangeRounded = () => {
           onChange={(e) => setRounded(parseInt(e.target.value))}
         />
         <span className="text-neutral-500">{rounded}px</span>
+      </div>
+    </div>
+  );
+};
+
+export const ChangeImgSize = () => {
+  const size = useImageSettings((state) => state.size);
+  const setSize = useImageSettings((state) => state.updateSize);
+  return (
+    <div className="flex flex-col space-y-1">
+      <label htmlFor="width">Size:</label>
+      <div className="flex items-center space-x-1">
+        <input
+          id="width"
+          type="number"
+          min="0"
+          maxLength={500}
+          value={size}
+          onChange={(e) => setSize(parseInt(e.target.value))}
+          className="w-40 rounded-md border border-neutral-700 px-2 py-1 focus:border-neutral-500 focus:outline-none dark:border-neutral-800 dark:bg-neutral-800 dark:focus:border-neutral-700"
+        />
+        <span className="text-neutral-500">px</span>
       </div>
     </div>
   );
