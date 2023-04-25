8430c1e35860ca43086a5472056fce2205b90fdc
pheralb <pablohdez4340@gmail.com>
2023-04-25 23:40:16 +0100

    💅 Add new gradients.

--- src/gradients/index.ts
+++ src/gradients/index.ts
@@ -8,4 +8,7 @@ export const gradients = [
   "bg-gradient-to-r from-yellow-200 via-green-200 to-green-500",
   "bg-gradient-to-r from-rose-300 to-rose-500",
   "bg-gradient-to-r from-red-200 via-red-300 to-yellow-200",
+  "bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-sky-400 to-indigo-900",
+  "Pastel bg-gradient-to-tr from-violet-500 to-orange-300",
+  "bg-gradient-to-r from-yellow-400 via-gray-50 to-teal-300",
 ];
