793db96137cc827e80249c687d1aa4db3ae4984a
pheralb <pablohdez4340@gmail.com>
2023-04-27 23:30:32 +0100

    ✨ Create spinner animation.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -25,8 +25,17 @@ module.exports = {
           from: { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
           to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
         },
+        spin: {
+          from: {
+            transform: "rotate(0deg)"
+          },
+          to: {
+            transform: "rotate(360deg)"
+          }
+        }
       },
       animation: {
+        animation: "spin 1s linear infinite",
         overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
         contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
       },
