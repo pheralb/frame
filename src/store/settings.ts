4504d0f2764553de7742bea571d9250bf306cb45
pheralb <pablohdez4340@gmail.com>
2023-04-27 23:32:18 +0100

    ⚙️ Update settings store.

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -4,8 +4,12 @@ import { devtools, persist } from "zustand/middleware";
 interface iBackgroundState {
   padding: number;
   rounded: number;
+  shadow: number;
+  shadowColor: string;
   updatePadding: (padding: number) => void;
   updateRounded: (rounded: number) => void;
+  updateShadow: (shadow: number) => void;
+  updateShadowColor: (shadowColor: string) => void;
 }
 
 export const useBackgroundSettings = create<iBackgroundState>()(
@@ -14,8 +18,12 @@ export const useBackgroundSettings = create<iBackgroundState>()(
       (set) => ({
         padding: 0,
         rounded: 0,
+        shadow: 0,
+        shadowColor: "#000000",
         updatePadding: (padding: number) => set({ padding }),
         updateRounded: (rounded: number) => set({ rounded }),
+        updateShadow: (shadow: number) => set({ shadow }),
+        updateShadowColor: (shadowColor: string) => set({ shadowColor }),
       }),
       {
         name: "background-settings-storage",
@@ -25,16 +33,20 @@ export const useBackgroundSettings = create<iBackgroundState>()(
 );
 
 interface iImageState {
-  size: number;
-  updateSize: (size: number) => void;
+  width: number;
+  height: number;
+  updateWidth: (width: number) => void;
+  updateHeight: (height: number) => void;
 }
 
 export const useImageSettings = create<iImageState>()(
   devtools(
     persist(
       (set) => ({
-        size: 0,
-        updateSize: (size: number) => set({ size }),
+        width: 0,
+        height: 0,
+        updateWidth: (width: number) => set({ width }),
+        updateHeight: (height: number) => set({ height }),
       }),
       {
         name: "image-settings-storage",
