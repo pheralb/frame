6977692894841f8e4a2aa429339bfbc88f0861f9
pheralb <pablohdez4340@gmail.com>
2023-04-29 18:07:28 +0100

    ⚙️ App improvements.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,51 @@
 import { useRef, useState } from "react";
+import { Resizable } from "re-resizable";
 import { cn, exportAsImage } from "@/utils";
 import { toast } from "sonner";
 import { AddMediaImage, Download } from "iconoir-react";
 
 import { gradients } from "@/gradients";
-import { Button, Dialog, Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui";
+import {
+  Input,
+  Button,
+  Dialog,
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+  TextArea,
+  ExternalLink,
+} from "@/ui";
 
 import Dropzone from "@/components/dropzone";
 import { Sidebar, SidebarSection } from "@/components/sidebar";
-import {
-  ChangeBgPadding,
-  ChangeBgRounded,
-  ChangeBgShadow,
-  ChangeBgShadowColor,
-} from "@/components/settings";
+import { ChangeBgPadding, ChangeBgRounded } from "@/components/settings";
 
-import { useBackgroundSettings, useImageSettings } from "@/store/settings";
+import { useBackgroundSettings } from "@/store/settings";
 
 export default function Home() {
-  const [gradient, setGradient] = useState<string>("");
-  const [name, setName] = useState<string>("");
-  const getImage = useRef<HTMLDivElement>(null);
+  const [name, setName] = useState<string>("My image");
   const [image, setImage] = useState<File | null>(null);
+  const getImage = useRef<HTMLDivElement>(null);
 
   // Background settings:
-  const { padding, rounded, shadow, shadowColor } = useBackgroundSettings(
+  const { padding, rounded, gradient, updateGradient } = useBackgroundSettings(
     (state) => ({
       padding: state.padding,
       rounded: state.rounded,
-      shadow: state.shadow,
-      shadowColor: state.shadowColor,
+      gradient: state.gradient,
+      updateGradient: state.updateGradient,
     })
   );
 
   const divStyle = {
     padding: `${padding}px`,
     borderRadius: `${rounded}px`,
-    boxShadow: `${shadow ? `0 0 ${shadow} ${shadowColor}` : "none"}`,
   };
 
-  // Image settings:
-  const { height } = useImageSettings((state) => ({
-    height: state.height,
-  }));
+  // Editor settings:
 
-  const imgSize = {
-    height: `${height}px`,
-  };
-
+  // Download image:
   const handleDownload = () => {
     exportAsImage(getImage.current as HTMLDivElement, name);
     toast("Downloading image...");
@@ -76,14 +74,21 @@ export default function Home() {
                   action={
                     <Button
                       icon={<Download width={18} stroke="1" />}
-                      onClick={handleDownload}
+                      onClick={() => handleDownload()}
                       className="w-full"
                     >
                       Download
                     </Button>
                   }
                 >
-                  asdas
+                  <form onSubmit={handleDownload}>
+                    <label className="text-sm font-medium">File name:</label>
+                    <Input
+                      className="mt-1 w-full"
+                      value={name}
+                      onChange={(e) => setName(e.target.value as string)}
+                    />
+                  </form>
                 </Dialog>
               </div>
             </SidebarSection>
@@ -95,46 +100,64 @@ export default function Home() {
                 </TabsList>
                 <TabsContent value="gallery">
                   <div className="flex flex-wrap items-center justify-center">
-                    {gradients.map((gradient) => (
+                    {gradients.map((gradientColor) => (
                       <button
-                        key={gradient}
-                        onClick={() => setGradient(gradient)}
+                        key={gradientColor}
+                        onClick={() => updateGradient(gradientColor)}
                         className={cn(
-                          "m-1 rounded-xl p-4 transition-all duration-150 hover:-translate-y-0.5",
-                          gradient
+                          "m-1 rounded-xl p-4 transition-all duration-150 hover:-translate-y-[1.25px]",
+                          gradientColor,
+                          gradient === gradientColor && "ring-2 ring-yellow-800"
                         )}
                       />
                     ))}
                   </div>
                 </TabsContent>
                 <TabsContent value="code">
-                  <textarea
-                    className="h-32 w-full rounded-xl bg-gray-100 p-2 dark:bg-gray-800"
+                  <TextArea
+                    className="mb-1 h-28 font-code"
                     placeholder="Paste your gradient here..."
                     value={gradient}
-                    onChange={(e) => setGradient(e.target.value)}
+                    onChange={(e) => updateGradient(e.target.value)}
                   />
+                  <p className="text-neutral-400">
+                    We are using Tailwind CSS to generate the gradients.{" "}
+                    <ExternalLink
+                      href="https://tailwindcss.com/docs/gradient-color-stops"
+                      underline={true}
+                      arrow={true}
+                    >
+                      Learn more
+                    </ExternalLink>
+                  </p>
                 </TabsContent>
               </Tabs>
             </SidebarSection>
-            <SidebarSection title="Background" border={false}>
+            <SidebarSection title="Background" border={true}>
               <div className="flex flex-col space-y-3">
                 <ChangeBgPadding />
                 <ChangeBgRounded />
-                <ChangeBgShadow />
-                <ChangeBgShadowColor />
+              </div>
+            </SidebarSection>
+            <SidebarSection title="Editor" border={false}>
+              <div className="flex items-center space-x-2">
+                <input type="checkbox" id="dots" onChange={() => {}} />
+                <label htmlFor="dots">Dots</label>
               </div>
             </SidebarSection>
           </Sidebar>
           <div className="m-3 ml-64 flex flex-col">
-            <div ref={getImage}>
-              <div style={divStyle} className={cn(gradient)}>
-                <img
-                  src={URL.createObjectURL(image)}
-                  alt="image"
-                  className="h-full w-full object-cover"
-                  style={imgSize}
-                />
+            <div className="border-2 border-dashed border-neutral-700">
+              <div ref={getImage}>
+                <Resizable>
+                  <div style={divStyle} className={cn(gradient)}>
+                    <img
+                      src={URL.createObjectURL(image)}
+                      alt="image"
+                      className="h-full w-full object-cover"
+                    />
+                  </div>
+                </Resizable>
               </div>
             </div>
           </div>
